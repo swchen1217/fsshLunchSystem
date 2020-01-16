d1aa56e37b422f66316555f35d87414e74c7279b
swchen1217 <swchen1217@gmail.com>
2020-01-16 10:28:46 +0800

    re

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,9 +27,9 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
+       /* if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
-        }
+		}*/
 
         return $this->respondWithToken($token);
     }
--- app/User.php
+++ app/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+	protected $table = 'users2';
+
     /**
      * The attributes that are mass assignable.
      *
