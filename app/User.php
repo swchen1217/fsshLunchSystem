a08a89e7f18218f95d747c42bd9b6acf2c75115f
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-07 22:13:17 +0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -11,8 +11,6 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
-	//protected $table = 'users2';
-
     /**
      * The attributes that are mass assignable.
      *
