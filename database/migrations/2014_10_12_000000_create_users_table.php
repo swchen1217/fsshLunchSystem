bd35f7ff47d3833e6b03b84f07ddcb7d4be8f25a
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-07 23:08:43 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('sid');
+            $table->increments('sid');
             $table->string('name');
             $table->string('password');
             $table->string('permission');
