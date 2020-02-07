543f0cee80b50c834bd0c473b34afb3c08efeff3
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-07 23:08:41 +0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'sid', 'password','permission'
     ];
 
     /**
@@ -34,9 +34,9 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array
      */
-    protected $casts = [
+    /*protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ];*/
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
