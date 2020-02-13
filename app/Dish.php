864b58b92a2318e5ce46ac656fa9441b3ed30f4c
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-13 23:25:07 +0800

    Update Dish.php

--- app/Dish.php
+++ app/Dish.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dish extends Model
 {
-    //id,saleDate,name,price,status,contents,calories,protein,fat,carbohydrate,note,photo
+    //id,saleDate,name,price,status,contents,calories,protein,fat,carbohydrate,stars,note,photo
 }
