1a84bbcd5dd8e42156747fcacd115dd88a162a60
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-13 23:01:00 +0800

    balance

---
+++ database/migrations/2020_02_13_144853_add_user_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            $table->integer('balance')->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            //can't be negative number
+            $table->dropColumn('balance');
+        });
+    }
+}
