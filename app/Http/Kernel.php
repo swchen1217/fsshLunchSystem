939929d6fc60f5f4c127349b182150bfc3d01a83
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-13 18:43:35 +0800

    laravel-permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,9 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,8 @@ Route::get('/', function () {
 Route::group([
     'prefix' => 'auth'
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-
 });
