a3efeceffad0ec3b12605ba2322f3b28f1854fdb
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-13 23:00:50 +0800

    dish

---
+++ app/Dish.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dish extends Model
+{
+    //id,saleDate,name,price,status,contents,calories,protein,fat,carbohydrate,note,photo
+}
