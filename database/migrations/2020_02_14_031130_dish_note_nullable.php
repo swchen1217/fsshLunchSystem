f655994cdcacdf4a2441a17300917d5055d2ac21
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-14 13:18:48 +0800

    dish

---
+++ database/migrations/2020_02_14_031130_dish_note_nullable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DishNoteNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->string('note')->default('')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->string('note')->change();
+        });
+
+    }
+}
