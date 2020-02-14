9171c85f37bbac2f03e6c244ae69b6c97b6fac6c
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-14 14:06:44 +0800

    dish

--- app/Dish.php
+++ app/Dish.php
@@ -9,4 +9,9 @@ class Dish extends Model
     //id,saleDate,factory,name,price,status,contents,calories,protein,fat,carbohydrate,stars,note,photo
 
     protected $guarded = ['id', 'photo'];
+
+    public function contents()
+    {
+        return $this->hasMany('App\DishContent','dish_id');
+    }
 }
