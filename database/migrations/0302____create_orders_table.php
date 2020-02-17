e4f07ee90a9645104cc21d71f669afe44535a129
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-18 00:57:25 +0800

    Update 0302____create_orders_table.php

--- database/migrations/0302____create_orders_table.php
+++ database/migrations/0302____create_orders_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateOrdersTable extends Migration
@@ -14,8 +15,14 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('dish_id');
+            $table->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreign('dish_id')->references('id')->on('dishes')->onDelete('CASCADE');
         });
     }
 
