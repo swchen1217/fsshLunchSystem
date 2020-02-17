fe611944a29a8fe0979ae88710fadbf3a7b7e508
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-17 21:48:00 +0800

    Create Dish.php

---
+++ app/Entity/Dish.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Exception;
+
+class Dish extends Model
+{
+    //id,saleDate,factory,name,price,status,contents,calories,protein,fat,carbohydrate,stars,note,photo
+
+    protected $guarded = ['id', 'photo'];
+
+    public function contents()
+    {
+        return $this->hasMany('App\DishContent');
+    }
+
+    public static function getDish()
+    {
+        $dish = Dish::all()->toArray();
+        foreach ($dish as $key => $value){
+            $dish[$key]=array_merge($value,array('contents'=>Dish::find($value['id'])->contents->toArray()));
+        }
+        return $dish;
+    }
+
+    public static function getDishById($id)
+    {
+        try{
+            $dish = Dish::findOrFail($id);
+            $tmp=array_merge($dish->toArray(),array('contents'=>Dish::find($id)->contents->toArray()));
+            return $tmp;
+        }catch (Exception $e){
+            throw $e;
+        }
+    }
+}
