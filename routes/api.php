357aa651fc08922addcbdb8a52c5bbe59f428ae6
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-18 23:27:44 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -30,5 +30,82 @@ Route::group([
     'prefix' => 'dish'
 ], function ($router) {
     Route::get('/', 'DishController@getDish');
-    Route::get('/id/{id}', 'DishController@getDishById');
+    Route::get('/{dish_id}', 'DishController@getDishById');
+    // TODO
+    Route::post('/', '');
+    Route::put('/{dish_id}', '');
+    Route::delete('/{dish_id}', '');
 });
+
+// TODO
+Route::group([
+    'prefix' => 'sale'
+], function ($router) {
+    Route::get('/', '');
+    Route::get('/id/{sale_id}', '');
+    Route::get('/date/{date}', '');
+    Route::post('/', '');
+    Route::put('/{sale_id}', '');
+    Route::delete('/{sale_id}', '');
+});
+
+Route::post('/top-up', '');
+Route::post('/refund', '');
+
+Route::group([
+    'prefix' => 'user'
+], function ($router) {
+    Route::get('/', '');
+    Route::get('/account/{account}', '');
+    Route::get('/class/{class}', '');
+    Route::post('/', '');
+    Route::put('/{account}', '');
+    Route::delete('/{account}', '');
+});
+
+Route::group([
+    'prefix' => 'order'
+], function ($router) {
+    Route::get('/', '');
+    Route::get('/id/{order_id}', '');
+    Route::get('/user/{user_id}', '');
+    Route::get('/dish/{dish_id}', '');
+    Route::get('/date/{date_id}', '');
+    Route::get('/manufacturer/{manufacturer_id}', '');
+    Route::get('/class/{class_id}', '');
+    Route::post('/', '');
+    Route::put('/{order_id}', '');
+    Route::delete('/{order_id}', '');
+});
+
+Route::group([
+    'prefix' => 'manufacturer'
+], function ($router) {
+    Route::get('/', '');
+    Route::post('/', '');
+    Route::put('/{manufacturer_id}', '');
+    Route::delete('/{manufacturer_id}', '');
+});
+
+Route::group([
+    'prefix' => 'rating'
+], function ($router) {
+    Route::get('/', '');
+    Route::get('/{dish_id}', '');
+    Route::post('/', '');
+    /*Route::put('/{_id}', '');
+    Route::delete('/{_id}', '');*/
+});
+
+/*
+
+Route::group([
+    'prefix' => ''
+], function ($router) {
+    Route::get('/{_id}', '');
+    Route::post('/', '');
+    Route::put('/{_id}', '');
+    Route::delete('/{_id}', '');
+});
+
+*/
