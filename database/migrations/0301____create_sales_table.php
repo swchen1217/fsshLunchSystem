683da911f9d83b5be377ff394512e5d86fc6fa5d
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-18 16:53:55 +0800

    Create 0301____create_sales_table.php

---
+++ database/migrations/0301____create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('sale_at');
+            $table->unsignedInteger('dish_id');
+            $table->unsignedInteger('status');
+
+            $table->foreign('dish_id')->references('id')->on('dishes')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
