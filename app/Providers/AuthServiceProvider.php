ad13f72b0024e4715e66e3587e3e83e7e4100014
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-19 11:52:53 +0800

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
+        Gate::after(function ($user, $ability) {
+            return $user->hasRole('Super Admin'); // note this returns boolean
+        });
     }
 }
