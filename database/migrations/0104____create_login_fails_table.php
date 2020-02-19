2443c08d6731bd4e5c2f2ab8c585f8ab9fec6ae2
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-19 23:55:38 +0800

    Create 0104____create_login_fails_table.php

---
+++ database/migrations/0104____create_login_fails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginFailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_fails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('ip');
+            $table->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_fails');
+    }
+}
