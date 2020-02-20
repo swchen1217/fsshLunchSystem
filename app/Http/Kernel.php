5ef25abf675c191bb62b4b84df14734fe96ccba8
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-20 12:53:01 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,7 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
-        'permission'    => \App\Http\Middleware\PermissionMiddleware::class,
+        'permission2'    => \App\Http\Middleware\PermissionMiddleware::class,
     ];
 
     /**
