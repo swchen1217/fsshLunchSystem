d949d3a95284275bb75192c52d7f238f642e1398
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-20 18:18:14 +0800

    Update User.php

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable implements JWTSubject
     use Notifiable;
     use HasRoles;
 
-    //protected $guard_name = 'api';
+    protected $guard_name = 'api';
 
     /**
      * The attributes that are mass assignable.
