a9ad537ed9566155431d9c9cb8d52a5b26ba418d
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-22 01:25:48 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('jwt.auth', ['except' => ['login']]);
+        $this->middleware('auth', ['except' => ['login']]);
     }
 
     /**
