d46107bb1b0913cf6b03b4015c814b8dc3634a4a
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-22 22:41:39 +0800

    Update User.php

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -10,22 +10,13 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Spatie\Permission\Traits\HasRoles;
 
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use Notifiable;
     use HasRoles;
     use HasApiTokens;
 
     protected $guard_name = 'api';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    /*protected $fillable = [
-        'name', 'id', 'password', 'permission', 'class', 'number', 'email'
-    ];*/
 
     /**
      * The attributes that should be hidden for arrays.
@@ -35,34 +26,5 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    /*protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];*/
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
 
