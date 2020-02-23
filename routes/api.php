371cac1cab85b3311fa89f36ef1a3bc588bb143d
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-23 18:05:49 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,18 @@ Route::get('/', function () {
     return response()->json(['success' => true]);
 });
 
+Route::get('/ip', function () {
+    return response()->json([
+        'HTTP_CLIENT_IP' =>$_SERVER['HTTP_CLIENT_IP']??'',
+        'HTTP_X_FORWARDED_FOR'=>$_SERVER['HTTP_X_FORWARDED_FOR']??'',
+        'HTTP_X_FORWARDED'=>$_SERVER['HTTP_X_FORWARDED']??'',
+        'HTTP_X_CLUSTER_CLIENT_IP'=>$_SERVER['HTTP_X_CLUSTER_CLIENT_IP']??'',
+        'HTTP_FORWARDED_FOR'=>$_SERVER['HTTP_FORWARDED_FOR']??'',
+        'HTTP_FORWARDED'=>$_SERVER['HTTP_FORWARDED']??'',
+        'REMOTE_ADDR'=>$_SERVER['REMOTE_ADDR']??'',
+        'HTTP_VIA'=>$_SERVER['HTTP_VIA']??'']);
+});
+
 Route::group([
     'prefix' => 'oauth'
 ], function ($router) {
