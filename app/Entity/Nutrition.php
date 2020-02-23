05433ab5243f4aa2a9c68cdd204553f5dd5b8dfe
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-23 22:58:14 +0800

    Update Nutrition.php

--- app/Entity/Nutrition.php
+++ app/Entity/Nutrition.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Nutrition extends Model
 {
     public $timestamps = false;
+
+    protected $fillable = ['calories','protein','fat','carbohydrate'];
 }
