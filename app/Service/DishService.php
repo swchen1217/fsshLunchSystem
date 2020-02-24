94a05307cc4a84a76594da14d2782d79c364bfd3
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-24 16:07:07 +0800

    Update DishService.php

--- app/Service/DishService.php
+++ app/Service/DishService.php
@@ -2,21 +2,15 @@
 
 namespace App\Service;
 
-use App\Repository\DishRepository;
+use App\Repositories\DishRepository;
 
 class DishService
 {
-
     private $dishRepository;
 
     public function __construct(DishRepository $dishRepository)
     {
         $this->dishRepository = $dishRepository;
     }
-
-    /*public function read($id)
-    {
-        return $this->post->find($id);
-    }*/
 
 }
