1c496840442add9c1feffbb6b40930c76cff90f5
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-24 20:18:43 +0800

    Create Login_failRepository.php

---
+++ app/Repositories/Login_failRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entity\Login_fail;
+
+class Login_failRepository
+{
+    /**
+     * @var Login_fail
+     */
+    private $login_fail;
+
+    public function __construct(Login_fail $login_fail)
+    {
+        $this->login_fail = $login_fail;
+    }
+
+    public function findByUserId($user_id)
+    {
+        return $this->login_fail->where('user_id',$user_id)->get();
+    }
+
+    public function findByUserIdAndIp($user_id,$ip)
+    {
+        return $this->login_fail->where('user_id',$user_id)->where('ip',$ip)->get();
+    }
+
+    public function caeate($data)
+    {
+        return $this->login_fail->create($data);
+    }
+
+    public function deleteByUserId($user_id)
+    {
+        return $this->login_fail->where('user_id',$user_id)->delete();
+    }
+}
