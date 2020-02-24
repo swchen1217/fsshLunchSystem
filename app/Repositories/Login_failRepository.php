acc0a5bd1607c202e02b1472e73d3393332d575d
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-24 23:10:28 +0800

    Update Login_failRepository.php

--- app/Repositories/Login_failRepository.php
+++ app/Repositories/Login_failRepository.php
@@ -31,9 +31,19 @@ class Login_failRepository
         return $this->login_fail->create($data);
     }
 
+    public function changeToUsedByUserIdAndIp($user_id,$ip)
+    {
+        return $this->login_fail->where('user_id',$user_id)->where('ip',$ip)->where('used',false)->update(['used',true]);
+    }
+
     public function deleteByUserId($user_id)
     {
         return $this->login_fail->where('user_id',$user_id)->delete();
+    }
+
+    public function deleteByUserIdAndIp($user_id,$ip)
+    {
+        return $this->login_fail->where('user_id',$user_id)->where('ip',$ip)->delete();
     }
 
 }
