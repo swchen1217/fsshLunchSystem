c62ad8ca18441d6766e7607b351c8effdf209135
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-25 19:11:39 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UserTableSeeder::class);
-        //$this->call(RBACSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(RBACSeeder::class);
 
-        //$this->call(ManufacturerSeeder::class);
+        $this->call(ManufacturerSeeder::class);
         $this->call(DishTableSeeder::class);
     }
 }
