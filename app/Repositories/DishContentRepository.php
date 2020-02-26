bae1e68d77d9bad5011271cfc45cb845e861f34a
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-26 20:55:22 +0800

    Update DishContentRepository.php

--- app/Repositories/DishContentRepository.php
+++ app/Repositories/DishContentRepository.php
@@ -15,4 +15,24 @@ class DishContentRepository
     {
         $this->dishContent = $dishContent;
     }
+
+    public function findById($id)
+    {
+        return $this->dishContent->find($id);
+    }
+
+    public function findByDishId($dish_id)
+    {
+        return $this->dishContent->where('dish_id',$dish_id);
+    }
+
+    public function caeate($data)
+    {
+        return $this->dishContent->create($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->dishContent->find($id)->delete();
+    }
 }
