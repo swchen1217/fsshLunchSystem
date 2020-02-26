94029aa5174fb4e4c0e9f43db1834e52873d35e7
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-26 23:33:31 +0800

    Create ManufacturerRepository.php

---
+++ app/Repositories/ManufacturerRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entity\Manufacturer;
+
+class ManufacturerRepository
+{
+    /**
+     * @var Manufacturer
+     */
+    private $manufacturer;
+
+    public function __construct(Manufacturer $manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+    }
+
+    public function findById($id)
+    {
+        return $this->manufacturer->find($id);
+    }
+
+    public function caeate($data)
+    {
+        return $this->manufacturer->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->manufacturer->where('id', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->manufacturer->find($id)->delete();
+    }
+}
