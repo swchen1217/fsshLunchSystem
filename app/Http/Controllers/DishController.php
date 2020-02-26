0fe73f07d37697f0c38fa758167959982789eacb
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-26 23:33:22 +0800

    Update DishController.php

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Entity\DishContent;
 use App\Repositories\DishInterface;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use App\Service\DishService;
+use Illuminate\Support\Facades\DB;
+use MongoDB\Driver\Exception\Exception;
 
 class DishController extends Controller
 {
@@ -13,26 +16,39 @@ class DishController extends Controller
     /**
      * @var DishService
      */
-    private $dishServiced;
+    private $dishService;
 
-    public function __construct(DishService $dishServiced)
+    public function __construct(DishService $dishService)
     {
-        //$this->middleware('auth:api', ['except' => ['getDishById','getDish']]);
-        $this->dishServiced = $dishServiced;
+        $this->middleware('auth:api');
+        $this->dishService = $dishService;
     }
 
     public function getDish()
     {
-        return response()->json(['isOk' => $this->dishServiced->test()], 200);
-        //return response()->json(['success'=>true,'data'=>Dish::getDish()],200);
+        $mResult=$this->dishService->getDish();
+        return response()->json($mResult[0],$mResult[1]);
     }
 
     public function getDishById(Request $request, $id)
     {
-        try {
-            return response()->json(['success' => true, 'data' => Dish::getDishById($id)], 200);
-        } catch (ModelNotFoundException $e) {
-            return response()->json(['success' => false, 'error' => 'The Dish Not Found'], 404);
-        }
+        $mResult=$this->dishService->getDish($id);
+        return response()->json($mResult[0],$mResult[1]);
+
+    }
+
+    public function newDish()
+    {
+
+    }
+
+    public function editDish()
+    {
+
+    }
+
+    public function removeDish()
+    {
+
     }
 }
