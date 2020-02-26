1fcfafc356f7da7e10a52b2177e30e34bbaccec5
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-26 20:55:26 +0800

    Update NutritionRepository.php

--- app/Repositories/NutritionRepository.php
+++ app/Repositories/NutritionRepository.php
@@ -15,4 +15,24 @@ class NutritionRepository
     {
         $this->nutrition = $nutrition;
     }
+
+    public function findById($id)
+    {
+        return $this->nutrition->find($id);
+    }
+
+    public function caeate($data)
+    {
+        return $this->nutrition->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->nutrition->where('id', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->nutrition->find($id)->delete();
+    }
 }
