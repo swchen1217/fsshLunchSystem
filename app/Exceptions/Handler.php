6a45d55286f34d694f012f7f61ff8b23971ea65a
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-27 20:44:02 +0800

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,10 +51,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        /*if ($exception instanceof ModelNotFoundException) {
-            return response()->json(['message' => 'Not Found!'], 404);
-        }*/
-
         return parent::render($request, $exception);
     }
 }
