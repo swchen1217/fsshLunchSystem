b34df46fae76f1241324169246e358af7ba4852a
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-28 19:27:15 +0800

    Update DishService.php

--- app/Service/DishService.php
+++ app/Service/DishService.php
@@ -6,8 +6,10 @@ use App\Repositories\DishContentRepository;
 use App\Repositories\DishRepository;
 use App\Repositories\ManufacturerRepository;
 use App\Repositories\NutritionRepository;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class DishService
 {
@@ -15,17 +17,20 @@ class DishService
     private $dishContentRepository;
     private $manufacturerRepository;
     private $nutritionRepository;
+    private $guzzleHttpClient;
 
     public function __construct(
         DishRepository $dishRepository,
         DishContentRepository $dishContentRepository,
         ManufacturerRepository $manufacturerRepository,
-        NutritionRepository $nutritionRepository)
+        NutritionRepository $nutritionRepository,
+        Client $client)
     {
         $this->dishRepository = $dishRepository;
         $this->dishContentRepository = $dishContentRepository;
         $this->manufacturerRepository = $manufacturerRepository;
         $this->nutritionRepository = $nutritionRepository;
+        $this->guzzleHttpClient = $client;
     }
 
     public function getDish($id = null)
@@ -69,16 +74,55 @@ class DishService
 
     public function newDish(Request $request)
     {
-        if(!$request->has('name') || !$request->has('manufacturer_id') || !$request->has('price') || !$request->has('calories') || !$request->has('protein') || !$request->has('fat') || !$request->has('carbohydrate')){
+        if (!$request->has('name') || !$request->has('manufacturer_id') || !$request->has('price') || !$request->has('calories') || !$request->has('protein') || !$request->has('fat') || !$request->has('carbohydrate')) {
             return [['error' => 'The request is incomplete'], Response::HTTP_BAD_REQUEST];
         }
-        if($this->manufacturerRepository->findById($request->manufacturer_id)==null){
+        if ($this->manufacturerRepository->findById($request->input('manufacturer_id')) == null) {
             return [['error' => 'The manufacturer_id error'], Response::HTTP_BAD_REQUEST];
         }
-        $nutrition_data=$request->only(['calories','protein','fat','carbohydrate']);
-        $nutrition_id=$this->nutritionRepository->caeate($nutrition_data);
-        $dish_data=$request->input(['naem','manufacturer_id','price']);
-        //$dish_id=$this->dishRepository->caeate($dish_data);
+        $nutrition_data = $request->only(['calories', 'protein', 'fat', 'carbohydrate']);
+        $nutrition = $this->nutritionRepository->caeate($nutrition_data);
+        $dish_data = array_merge($request->only(['name', 'manufacturer_id', 'price']), ['nutrition_id' => $nutrition->id, 'photo' => Storage::disk('public')->url('image/dish/default.png')]);
+        $dish = $this->dishRepository->caeate($dish_data);
+        $contents_data = $request->input('contents');
+        foreach ($contents_data as $item)
+            $this->dishContentRepository->caeate(['dish_id' => $dish->id, 'name' => $item]);
+        return $this->getDish($dish->id);
     }
 
+    public function image(Request $request){
+        if(!$request->has('dish_id') || $this->dishRepository->findById($request->input('dish_id'))==null)
+            return [['error' => '`dish_id` Not Found'],Response::HTTP_NOT_FOUND];
+        $dish_id=$request->input('dish_id');
+        if($request->input('type')=='url'){
+            $photo_url=$request->input('url');
+        }elseif ($request->input('type')=='image'){
+            if($request->hasFile('image')){
+                $response = $this->guzzleHttpClient->request('POST', 'https://api.imgur.com/3/image', [
+                    'headers' => [
+                        'authorization' => 'Bearer ' . env('IMGUR_ACCESS_TOKEN'),
+                        'content-type' => 'application/x-www-form-urlencoded',
+                    ],
+                    'form_params' => [
+                        'image' => base64_encode(file_get_contents($request->file('image')->path())),
+                        'title'=>'dish_'.$dish_id
+                    ],
+                ]);
+                $photo_url=json_decode($response->getBody()->getContents(),true)['data']['link'];
+            }else
+                return [['error' => 'Image Not Found'],Response::HTTP_BAD_REQUEST0];
+        }else{
+            return [['error' => 'The type is empty or not supported'],Response::HTTP_BAD_REQUEST0];
+        }
+        $this->dishRepository->update($dish_id,['photo'=>$photo_url]);
+        return [[],Response::HTTP_NO_CONTENT];
+    }
+
+    public function editDish(Request $request){
+        
+    }
+
+    public function removeDish(Request $request){
+
+    }
 }
