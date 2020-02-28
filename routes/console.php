5dde0b6003149387f479bca673634ce567e98f85
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-29 00:50:08 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,8 @@
 
 use App\Mail\Verify;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
@@ -22,8 +24,8 @@ Artisan::command('inspire', function () {
 })->describe('Display an inspiring quote');
 
 Artisan::command('log:clear', function () {
-    $path=storage_path('logs\laravel.log');
-    if(is_file($path)){
+    $path = storage_path('logs\laravel.log');
+    if (is_file($path)) {
         unlink($path);
         fclose(fopen($path, "w"));
     }
@@ -31,16 +33,26 @@ Artisan::command('log:clear', function () {
 });
 
 Artisan::command('my:test', function () {
-    /*Mail::queue('emails.user_verify', ['verify_code'=>'12345678'], function ($message) {
-        $message->to('swchen1217@gmail.com', 'SWC')->subject('帳號活動異常-使用者驗證');
-    });*/
-    $user=App\Entity\User::find(1);
-    Mail::to($user)->queue(new Verify(['verify_code'=>'12345678']));
+    for ($i = 0; $i < 10; $i++)
+        \App\Entity\Rating::create(['user_id' => '1', 'dish_id' => '2', 'rating' => '3', 'created_at' => Carbon::now()->addDays($i + 1)->toDateString()]);
     $this->info('ok');
 });
 
 Artisan::command('my:getImage', function () {
     $url = Storage::disk('public')->url('image/dish/default.png');
     $this->line($url);
+    $this->info('ok');
+});
+
+Artisan::command('my:test2', function () {
+    $id = 3;
+    Cache::tags('rating')->flush();
+    $result = Cache::tags('rating')->remember($id, Carbon::now()->addHours(3), function () use ($id) {
+        $rating = \App\Entity\Rating::where('dish_id', $id)->get();
+        if ($rating == null || count($rating) < 10)
+            return -1;
+        return round($rating->avg('rating'), 1);
+    });
+    $this->line($result);
     $this->info('ok');
 });
