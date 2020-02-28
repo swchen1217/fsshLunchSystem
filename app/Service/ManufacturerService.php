65e1975f81bd3e412557e474cf09ed53bdd33c7b
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-29 03:19:45 +0800

    Update ManufacturerService.php

--- app/Service/ManufacturerService.php
+++ app/Service/ManufacturerService.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use App\Repositories\ManufacturerRepository;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ManufacturerService
 {
@@ -14,5 +16,33 @@ class ManufacturerService
     public function __construct(ManufacturerRepository $manufacturerRepository)
     {
         $this->manufacturerRepository = $manufacturerRepository;
+    }
+
+    public function get()
+    {
+        return [$this->manufacturerRepository->all(),Response::HTTP_OK];
+    }
+
+    public function create(Request $request)
+    {
+        return [$this->manufacturerRepository->caeate($request->only(['name'])),Response::HTTP_CREATED];
+    }
+
+    public function edit(Request $request, $manufacturer_id)
+    {
+        $edit=$this->manufacturerRepository->update($manufacturer_id,$request->only(['name']));
+        if($edit!=0)
+            return [$this->manufacturerRepository->findById($manufacturer_id),Response::HTTP_OK];
+        else
+            return [['error' => 'The Manufacturer Not Found'], Response::HTTP_NOT_FOUND];
+    }
+
+    public function remove(Request $request, $manufacturer_id)
+    {
+        if($this->manufacturerRepository->findById($manufacturer_id)!=null)
+            return [[], Response::HTTP_NO_CONTENT];
+
+        else
+            return [['error' => 'The Manufacturer Not Found'], Response::HTTP_NOT_FOUND];
     }
 }
