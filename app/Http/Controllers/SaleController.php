57e65d1e59ef2369d3f932a32e4a4204c9ccd3a0
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-01 00:35:19 +0800

    Update SaleController.php

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -17,4 +17,40 @@ class SaleController extends Controller
         $this->middleware('auth:api');
         $this->saleService = $saleService;
     }
+
+    public function getAll()
+    {
+        $mResult = $this->saleService->getSaleData();
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function getById(Request $request, $sale_id)
+    {
+        $mResult = $this->saleService->getSaleData('id',$sale_id);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function getBySaleDate(Request $request, $saleDate)
+    {
+        $mResult = $this->saleService->getSaleData('saleDate',$saleDate);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function create(Request $request)
+    {
+        $mResult = $this->saleService->create($request);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function edit(Request $request, $sale_id)
+    {
+        $mResult = $this->saleService->edit($request,$sale_id);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function remove(Request $request, $sale_id)
+    {
+        $mResult = $this->saleService->remove($request,$sale_id);
+        return response()->json($mResult[0], $mResult[1]);
+    }
 }
