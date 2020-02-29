9f2dcf85432c819ac3dddb5c5aa572b7d194a70a
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-01 00:35:32 +0800

    Update SaleRepository.php

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -15,4 +15,34 @@ class SaleRepository
     {
         $this->sale = $sale;
     }
+
+    public function all()
+    {
+        return $this->sale->all();
+    }
+
+    public function findById($id)
+    {
+        return $this->sale->find($id);
+    }
+
+    public function findBySaleDate($date)
+    {
+        return $this->sale->where('sale_at',$date)->get();
+    }
+
+    public function caeate($data)
+    {
+        return $this->sale->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->sale->where('id', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->sale->find($id)->delete();
+    }
 }
