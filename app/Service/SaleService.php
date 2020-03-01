b5d9ca24c5316086feeedfdcd24640a3f6e833d7
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-01 11:23:39 +0800

    Update SaleService.php

--- app/Service/SaleService.php
+++ app/Service/SaleService.php
@@ -5,6 +5,8 @@ namespace App\Service;
 use App\Repositories\SaleRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class SaleService
 {
@@ -27,18 +29,26 @@ class SaleService
     {
         if ($type != 'id') {
             if ($type == 'all') {
-                $sale = $this->saleRepository->all();
+                $key = 'all';
             } elseif ($type == 'saleDate') {
-                $sale = $this->saleRepository->findBySaleDate($data);
+                $key = 'date_' . $data;
             }
-            $result = array();
-            foreach ($sale as $item) {
-                $ss = $item->toArray();
-                $dish_id = $ss['dish_id'];
-                array_splice($ss, 2, 1);
-                $result[] = array_merge($ss, ['dish' => $this->dishService->getDish($dish_id)[0]]);
-            }
-            return [$result, Response::HTTP_OK];
+            $sale_date = Cache::tags('sale')->remember($key, Carbon::now()->addHours(3), function () use ($type, $data) {
+                if ($type == 'all') {
+                    $sale = $this->saleRepository->all();
+                } elseif ($type == 'saleDate') {
+                    $sale = $this->saleRepository->findBySaleDate($data);
+                }
+                $result = array();
+                foreach ($sale as $item) {
+                    $ss = $item->toArray();
+                    $dish_id = $ss['dish_id'];
+                    array_splice($ss, 2, 1);
+                    $result[] = array_merge($ss, ['dish' => $this->dishService->getDish($dish_id)[0]]);
+                }
+                return serialize($result);
+            });
+            return [unserialize($sale_date), Response::HTTP_OK];
         } else {
             $sale = $this->saleRepository->findById($data);
             if ($sale == null)
@@ -53,16 +63,16 @@ class SaleService
 
     public function create(Request $request)
     {
-
+        Cache::tags('sale')->flush();
     }
 
     public function edit(Request $request, $manufacturer_id)
     {
-
+        Cache::tags('sale')->flush();
     }
 
     public function remove(Request $request, $manufacturer_id)
     {
-
+        Cache::tags('sale')->flush();
     }
 }
