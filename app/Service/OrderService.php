673afa90f3c28cfb68811bdf2030d863a7ea4d39
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 21:45:41 +0800

    Update OrderService.php

--- app/Service/OrderService.php
+++ app/Service/OrderService.php
@@ -11,7 +11,9 @@ use App\Supports\PermissionSupport;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Expr\Cast\Object_;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 
 class OrderService
 {
@@ -52,79 +54,97 @@ class OrderService
 
     public function get($type = 'all', $data = null, Request $request = null)
     {
-        if($type!='order_id'){
-            if($type=='all') //order.read
+        if ($type != 'order_id') {
+            if ($type == 'all' && PermissionSupport::check('order.read', null, true)) //order.read
                 $order = $this->orderRepository->all();
-            elseif ($type=='user_id') //order.read.self
-                $order = $this->orderRepository->findByUserId($data);
-            elseif ($type=='sale_id') //order.read
+            elseif ($type == 'user_id') { //order.read.self
+                if($this->getByUserIdCheck($data))
+                    $order = $this->orderRepository->findByUserId($data);
+                /*if (PermissionSupport::check('order.read'))
+                    $order = $this->orderRepository->findByUserId($data);
+                elseif ($data == Auth::user()->id && PermissionSupport::check('order.read.self', null, true))
+                    $order = $this->orderRepository->findByUserId($data);
+                elseif (PermissionSupport::check('order.read.class', null, true)) {
+                    $pass = false;
+                    $class_user = $this->userRepository->findByClass(Auth::user()->class);
+                    foreach ($class_user as $cu) {
+                        if ($data == $cu->id) {
+                            $pass = true;
+                            break;
+                        }
+                    }
+                    if ($pass)
+                        $order = $this->orderRepository->findByUserId($data);
+                    else
+                        throw UnauthorizedException::forPermissions(['order.read.class']);
+                }*/
+            } elseif ($type == 'sale_id' && PermissionSupport::check('order.read', null, true)) //order.read
                 $order = $this->orderRepository->findBySaleId($data);
-            elseif ($type=='saleDate'){ //order.read
+            elseif ($type == 'saleDate' && PermissionSupport::check('order.read', null, true)) { //order.read
                 $sale = $this->saleRepository->findBySaleDate($data);
-                $order=collect();
-                foreach ($sale as $item){
-                    $ss=$this->orderRepository->findBySaleId($item->id);
+                $order = collect();
+                foreach ($sale as $item) {
+                    $ss = $this->orderRepository->findBySaleId($item->id);
                     foreach ($ss as $s)
                         $order->push($s);
                 }
-            }elseif ($type=='manufacturer_id'){ //order.read
-                $dish=$this->dishRepository->findByManufacturer_id($data);
-                $order=collect();
-                foreach ($dish as $dd){
-                    $ss=$this->saleRepository->findByDishId($dd->id);
-                    if($ss!=null){
-                        foreach ($ss as $s){
-                            $oo=$this->orderRepository->findBySaleId($s->id);
+            } elseif ($type == 'manufacturer_id' && PermissionSupport::check('order.read', null, true)) { //order.read
+                $dish = $this->dishRepository->findByManufacturer_id($data);
+                $order = collect();
+                foreach ($dish as $dd) {
+                    $ss = $this->saleRepository->findByDishId($dd->id);
+                    if ($ss != null) {
+                        foreach ($ss as $s) {
+                            $oo = $this->orderRepository->findBySaleId($s->id);
                             foreach ($oo as $o)
                                 $order->push($o);
                         }
                     }
                 }
-            }elseif ($type=='class'){ //order.read.class
-                $user=$this->userRepository->findByClass($data);
-                $order=collect();
-                foreach ($user as $uu){
-                    $oo=$this->orderRepository->findByUserId($uu->id);
+            } elseif ($type == 'class' && PermissionSupport::check('order.read.class', null, true)) { //order.read.class
+                $user = $this->userRepository->findByClass($data);
+                $order = collect();
+                foreach ($user as $uu) {
+                    $oo = $this->orderRepository->findByUserId($uu->id);
                     foreach ($oo as $o)
                         $order->push($o);
                 }
-            }elseif ($type=='classToday'){ //order.read.class.today
-                $user=$this->userRepository->findByClass($data);
-                $order=collect();
-                foreach ($user as $uu){
-                    $oo=$this->orderRepository->findByUserId($uu->id);
-                    foreach ($oo as $o){
-                        $s=$this->saleRepository->findById($o->sale_id);
-                        if(Carbon::today()->eq(Carbon::parse($s->sale_at)))
+            } elseif ($type == 'classToday' && PermissionSupport::check('order.read.class.today', null, true)) { //order.read.class.today
+                $user = $this->userRepository->findByClass($data);
+                $order = collect();
+                foreach ($user as $uu) {
+                    $oo = $this->orderRepository->findByUserId($uu->id);
+                    foreach ($oo as $o) {
+                        $s = $this->saleRepository->findById($o->sale_id);
+                        if (Carbon::today()->eq(Carbon::parse($s->sale_at)))
                             $order->push($o);
                     }
                 }
             }
-
             $result = array();
             foreach ($order as $item) {
                 $user = $this->userRepository->findById($item->user_id)->only(['id', 'account', 'class', 'number']);
                 $sale = $this->saleRepository->findById($item->sale_id)->only(['id', 'sale_at', 'dish_id']);
-                $dish_id=$sale['dish_id'];
+                $dish_id = $sale['dish_id'];
                 array_splice($sale, 2, 1);
                 $dish = $this->dishRepository->findById($dish_id)->only(['id', 'name', 'manufacturer_id', 'price']);
                 $manufacturer = $this->manufacturerRepository->findById($dish['manufacturer_id']);
-                $result[] = array_merge(['order_id' => $item->id, 'user' => $user, 'sale'=>array_merge($sale,['dish'=>array_merge($dish,['manufacturer_name'=>$manufacturer->name])])]);
+                $result[] = array_merge(['order_id' => $item->id, 'user' => $user, 'sale' => array_merge($sale, ['dish' => array_merge($dish, ['manufacturer_name' => $manufacturer->name])])]);
             }
 
-            return [$result,Response::HTTP_OK];
-        }else{
+            return [$result, Response::HTTP_OK];
+        } else {
             $order = $this->orderRepository->findById($data);
-            if($order!=null){
+            if ($order != null && $this->getByUserIdCheck($order->user_id)) {
                 $user = $this->userRepository->findById($order->user_id)->only(['id', 'account', 'class', 'number']);
                 $sale = $this->saleRepository->findById($order->sale_id)->only(['id', 'sale_at', 'dish_id']);
-                $dish_id=$sale['dish_id'];
+                $dish_id = $sale['dish_id'];
                 array_splice($sale, 2, 1);
                 $dish = $this->dishRepository->findById($dish_id)->only(['id', 'name', 'manufacturer_id', 'price']);
                 $manufacturer = $this->manufacturerRepository->findById($dish['manufacturer_id']);
-                $result = array_merge(['order_id' => $order->id, 'user' => $user, 'sale'=>array_merge($sale,['dish'=>array_merge($dish,['manufacturer_name'=>$manufacturer->name])])]);
-                return [$result,Response::HTTP_OK];
-            }else
+                $result = array_merge(['order_id' => $order->id, 'user' => $user, 'sale' => array_merge($sale, ['dish' => array_merge($dish, ['manufacturer_name' => $manufacturer->name])])]);
+                return [$result, Response::HTTP_OK];
+            } else
                 return [['error' => 'The Order Not Found'], Response::HTTP_NOT_FOUND];
         }
     }
@@ -146,5 +166,22 @@ class OrderService
         //self
         //class
         //all
+    }
+
+    private function getByUserIdCheck($user_id){
+        if (PermissionSupport::check('order.read'))
+            return true;
+        elseif ($user_id == Auth::user()->id && PermissionSupport::check('order.read.self', null, true))
+            return true;
+        elseif (PermissionSupport::check('order.read.class', null, true)) {
+            $class_user = $this->userRepository->findByClass(Auth::user()->class);
+            foreach ($class_user as $cu) {
+                if ($user_id == $cu->id) {
+                    return true;
+                }
+            }
+            throw UnauthorizedException::forPermissions(['order.read.class']);
+        }
+        return false;
     }
 }
