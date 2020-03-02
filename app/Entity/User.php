ecd86f84e65dd6d362ae479c83d1ee0e494eb46f
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 22:08:42 +0800

    Update User.php

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
 
     protected $guard_name = 'api';
 
-    protected $guarded=['id'];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for arrays.
