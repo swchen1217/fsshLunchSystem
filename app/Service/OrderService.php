7c07632fccc0760b9ca2a243c70e250aa196c95f
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 19:14:43 +0800

    Update OrderService.php

--- app/Service/OrderService.php
+++ app/Service/OrderService.php
@@ -10,6 +10,7 @@ use App\Repositories\UserRepository;
 use App\Supports\PermissionSupport;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class OrderService
 {
@@ -50,8 +51,24 @@ class OrderService
 
     public function get($type = 'all', $data = null, Request $request = null)
     {
-        if (PermissionSupport::check('order.read', null, true)) {
-            $order = $this->orderRepository->all();
+
+        if($type!='order_id'){
+            if($type=='all')
+                $order = $this->orderRepository->all();
+            elseif ($type=='user_id')
+                $order = $this->orderRepository->findByUserId($data);
+            elseif ($type=='sale_id')
+                $order = $this->orderRepository->findBySaleId($data);
+            elseif ($type=='saleDate'){
+                $sale = $this->saleRepository->findBySaleDate($data);
+                $order=collect();
+                foreach ($sale as $item){
+                    $order->push($this->orderRepository->findBySaleId($item->id));
+                }
+            }
+
+
+
             $result = array();
             foreach ($order as $item) {
                 $user = $this->userRepository->findById($item->user_id)->only(['id', 'account', 'class', 'number']);
@@ -60,9 +77,23 @@ class OrderService
                 array_splice($sale, 2, 1);
                 $dish = $this->dishRepository->findById($dish_id)->only(['id', 'name', 'manufacturer_id', 'price']);
                 $manufacturer = $this->manufacturerRepository->findById($dish['manufacturer_id']);
-                    $result[] = array_merge(['order_id' => $item->id, 'user' => $user, 'sale'=>array_merge($sale,['dish'=>array_merge($dish,['manufacturer_name'=>$manufacturer->name])])]);
+                $result[] = array_merge(['order_id' => $item->id, 'user' => $user, 'sale'=>array_merge($sale,['dish'=>array_merge($dish,['manufacturer_name'=>$manufacturer->name])])]);
             }
+
             return [$result,Response::HTTP_OK];
+        }else{
+            $order = $this->orderRepository->findById($data);
+            if($order!=null){
+                $user = $this->userRepository->findById($order->user_id)->only(['id', 'account', 'class', 'number']);
+                $sale = $this->saleRepository->findById($order->sale_id)->only(['id', 'sale_at', 'dish_id']);
+                $dish_id=$sale['dish_id'];
+                array_splice($sale, 2, 1);
+                $dish = $this->dishRepository->findById($dish_id)->only(['id', 'name', 'manufacturer_id', 'price']);
+                $manufacturer = $this->manufacturerRepository->findById($dish['manufacturer_id']);
+                $result = array_merge(['order_id' => $order->id, 'user' => $user, 'sale'=>array_merge($sale,['dish'=>array_merge($dish,['manufacturer_name'=>$manufacturer->name])])]);
+                return [$result,Response::HTTP_OK];
+            }else
+                return [['error' => 'The Order Not Found'], Response::HTTP_NOT_FOUND];
         }
 
 
