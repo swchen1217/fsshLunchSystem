8694cd9bb8fdc77920fc9d819322424d138b535b
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 22:09:19 +0800

    Update DishTableSeeder.php

--- database/seeds/DishTableSeeder.php
+++ database/seeds/DishTableSeeder.php
@@ -20,27 +20,27 @@ class DishTableSeeder extends Seeder
             DB::table('dish_contents')->delete();*/
         if (Dish::all()->count() != 0)
             DB::table('dishes')->delete();
-        $price=[50,55];
-        $start_date="2020-03-02";
-        $date=strtotime($start_date);
-        $times=3;
-        $faker=Faker\Factory::create();
+        $price = [50, 55];
+        $start_date = "2020-03-02";
+        $date = strtotime($start_date);
+        $times = 3;
+        $faker = Faker\Factory::create();
         $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
-        for($n=0;$n<$times;$n++){
-            for($d=0;$d<5;$d++){
-                for($f=0;$f<3;$f++){
-                    for($i=0;$i<2;$i++){
-                        $nid=Nutrition::create(['calories'=>rand(750,850),'protein'=>rand(40,50),'fat'=>rand(16,18),'carbohydrate'=>rand(115,125)]);
-                        $did=Dish::create(['name'=>$name=$faker->foodName(),'manufacturer_id'=>$f+1,'nutrition_id'=>$nid->id,'price'=>$price[$i],'photo'=>date('YmdHis',time()).'_'.substr(md5($name),0,6)]);
-                        for($c=0;$c<rand(3,7);$c++){
-                            $r=rand(0,2);
-                            DishContent::create(['dish_id'=>$did->id,'name'=>$r==0?$faker->vegetableName():($r==1?$faker->meatName():$faker->sauceName())]);
+        for ($n = 0; $n < $times; $n++) {
+            for ($d = 0; $d < 5; $d++) {
+                for ($f = 0; $f < 3; $f++) {
+                    for ($i = 0; $i < 2; $i++) {
+                        $nid = Nutrition::create(['calories' => rand(750, 850), 'protein' => rand(40, 50), 'fat' => rand(16, 18), 'carbohydrate' => rand(115, 125)]);
+                        $did = Dish::create(['name' => $name = $faker->foodName(), 'manufacturer_id' => $f + 1, 'nutrition_id' => $nid->id, 'price' => $price[$i], 'photo' => date('YmdHis', time()) . '_' . substr(md5($name), 0, 6)]);
+                        for ($c = 0; $c < rand(3, 7); $c++) {
+                            $r = rand(0, 2);
+                            DishContent::create(['dish_id' => $did->id, 'name' => $r == 0 ? $faker->vegetableName() : ($r == 1 ? $faker->meatName() : $faker->sauceName())]);
                         }
                     }
                 }
-                $date= strtotime("+1 day",$date);
+                $date = strtotime("+1 day", $date);
             }
-            $date= strtotime("+2 day",$date);
+            $date = strtotime("+2 day", $date);
         }
     }
 }
