b39bb845abbede60eca46f5cd2b472bb8111534c
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 22:02:41 +0800

    Update BalanceRepository.php

--- app/Repositories/BalanceRepository.php
+++ app/Repositories/BalanceRepository.php
@@ -1,1 +1,53 @@
 <?php
+
+namespace App\Repositories;
+
+use App\Entity\Balance;
+
+class BalanceRepository
+{
+    /**
+     * @var Balance
+     */
+    private $balance;
+
+    public function __construct(Balance $balance)
+    {
+        $this->balance = $balance;
+    }
+
+    public function findById($id)
+    {
+        return $this->balance->find($id);
+    }
+
+    public function findByUserId($user_id)
+    {
+        return $this->balance->where('user_id',$user_id)->get();
+    }
+
+    public function caeate($data)
+    {
+        return $this->balance->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        return $this->balance->where('id', $id)->update($data);
+    }
+
+    public function updateByUserId($id, $data)
+    {
+        return $this->balance->where('id', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->balance->where('id', $id)->delete();
+    }
+
+    public function deleteByUserId($id)
+    {
+        return $this->balance->where('id', $id)->delete();
+    }
+}
