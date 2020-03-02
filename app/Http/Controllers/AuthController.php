07ff5a5c1b0143d7877cef66f43c2772d048f80c
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 22:08:46 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,31 +20,31 @@ class AuthController extends Controller
 
     public function __construct(AuthService $authService)
     {
-        $this->middleware('auth:api', ['except' => ['createToken','verifyCommit']]);
-        $this->authService=$authService;
+        $this->middleware('auth:api', ['except' => ['createToken', 'verifyCommit']]);
+        $this->authService = $authService;
     }
 
     public function createToken(Request $request)
     {
-        $mResult=$this->authService->createToken($request);
-        return response()->json($mResult[0],$mResult[1]);
+        $mResult = $this->authService->createToken($request);
+        return response()->json($mResult[0], $mResult[1]);
     }
 
     public function verifyCommit(Request $request)
     {
-        $mResult=$this->authService->verifyCommit($request);
-        return response()->json($mResult[0],$mResult[1]);
+        $mResult = $this->authService->verifyCommit($request);
+        return response()->json($mResult[0], $mResult[1]);
     }
 
     public function user(Request $request)
     {
-        $mResult=$this->authService->getUser($request);
-        return response()->json($mResult[0],$mResult[1]);
+        $mResult = $this->authService->getUser($request);
+        return response()->json($mResult[0], $mResult[1]);
     }
 
     public function revokeToken(Request $request, $tokenId)
     {
-        $mResult=$this->authService->revokeToken($request,$tokenId);
-        return response()->json($mResult[0],$mResult[1]);
+        $mResult = $this->authService->revokeToken($request, $tokenId);
+        return response()->json($mResult[0], $mResult[1]);
     }
 }
