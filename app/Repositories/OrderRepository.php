90e30e97465cd4781dcefd64aeafeff7419aa989
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-02 19:14:40 +0800

    Update OrderRepository.php

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -26,6 +26,16 @@ class OrderRepository
         return $this->order->find($id);
     }
 
+    public function findByUserId($user_id)
+    {
+        return $this->order->where('user_id',$user_id)->get();
+    }
+
+    public function findBySaleId($sale_id)
+    {
+        return $this->order->where('sale_id',$sale_id)->get();
+    }
+
     public function caeate($data)
     {
         return $this->order->create($data);
