d98505af4de76887921a97e4a60cda7f35fca582
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 23:04:26 +0800

    Update URLConstant.php

--- app/Constant/URLConstant.php
+++ app/Constant/URLConstant.php
@@ -4,8 +4,8 @@ namespace App\Constant;
 
 class URLConstant
 {
-    const URL_DEV_FRONTEND_FORGET_PW='http://fios.cf/forget-password';
-    const URL_DEV_ADMIN_FRONTEND_FORGET_PW='http://swc1217.tk/FIOS_Frontend_Admin/index.html#ChangePW';
-    const URL_PDC_FRONTEND_FORGET_PW='http://fios.fssh.khc.edu.tw/forget-password';
-    const URL_PDC_ADMIN_FRONTEND_FORGET_PW='http://admin.fios.fssh.khc.edu.tw/index.html#ChangePW';
+    const URL_DEV_FRONTEND_FORGET_PW = 'http://fios.cf/forget-password';
+    const URL_DEV_ADMIN_FRONTEND_FORGET_PW = 'http://swc1217.tk/FIOS_Frontend_Admin/index.html#ChangePW';
+    const URL_PDC_FRONTEND_FORGET_PW = 'http://fios.fssh.khc.edu.tw/forget-password';
+    const URL_PDC_ADMIN_FRONTEND_FORGET_PW = 'http://admin.fios.fssh.khc.edu.tw/index.html#ChangePW';
 }
