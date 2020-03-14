06f78ed85f77074111eb2765d7ec719aea8687e4
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 20:40:10 +0800

    Update GeneralController.php

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -6,13 +6,27 @@ use Illuminate\Http\Request;
 
 class GeneralController extends Controller
 {
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
     public function isOk()
     {
         return response()->json(['success' => true]);
     }
 
-    public function welcome()
+    public function ip(Request $request)
     {
-        return view('welcome');
+        return response()->json([
+            'HTTP_CLIENT_IP' => $_SERVER['HTTP_CLIENT_IP'] ?? '',
+            'HTTP_X_FORWARDED_FOR' => $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '',
+            'HTTP_X_FORWARDED' => $_SERVER['HTTP_X_FORWARDED'] ?? '',
+            'HTTP_X_CLUSTER_CLIENT_IP' => $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'] ?? '',
+            'HTTP_FORWARDED_FOR' => $_SERVER['HTTP_FORWARDED_FOR'] ?? '',
+            'HTTP_FORWARDED' => $_SERVER['HTTP_FORWARDED'] ?? '',
+            'REMOTE_ADDR' => $_SERVER['REMOTE_ADDR'] ?? '',
+            'HTTP_VIA' => $_SERVER['HTTP_VIA'] ?? '',
+            'Request::ip()' => $request->ip()]);
     }
 }
