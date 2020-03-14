4852c32ca5060ea5fae17c420a8859e5b9700546
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 23:03:51 +0800

    Create ForgetPswd.php

---
+++ app/Entity/ForgetPswd.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ForgetPswd extends Model
+{
+    public $timestamps = false;
+
+    protected $guarded = ['id'];
+}
