3c39b18d98711d3572b066527eebc9f452b1a104
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 21:43:13 +0800

    Update PswdController.php

--- app/Http/Controllers/PswdController.php
+++ app/Http/Controllers/PswdController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\PswdService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class PswdController extends Controller
 {
-    //
+    /**
+     * @var PswdService
+     */
+    private $pswdService;
+
+    public function __construct(PswdService $pswdService)
+    {
+        $this->pswdService = $pswdService;
+    }
+
+    public function account(Request $request, $account)
+    {
+        $mResult = $this->pswdService->account($request, $account);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function forget(Request $request, $email)
+    {
+        $validator = Validator::make([$email], ['required|email']);
+        if ($validator->fails())
+            return response()->json(['error' => 'Email format error'], Response::HTTP_BAD_REQUEST);
+        $mResult = $this->pswdService->forget($request, $email);
+        return response()->json($mResult[0], $mResult[1]);
+    }
+
+    public function token(Request $request, $token)
+    {
+        $mResult = $this->pswdService->token($request, $token);
+        return response()->json($mResult[0], $mResult[1]);
+    }
 }
