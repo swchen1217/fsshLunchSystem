c3efcca9ed22ee7402804e855fc7ad56ef5a413f
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 21:43:16 +0800

    Create PswdService.php

---
+++ app/Service/PswdService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PswdService
+{
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function account(Request $request, $account)
+    {
+
+    }
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function forget(Request $request, $email)
+    {
+
+    }
+
+    public function token(Request $request, $token)
+    {
+
+    }
+}
