6f0a577653dbc68580c1fc93333d1ca6c9c4018b
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 23:04:03 +0800

    Update PswdService.php

--- app/Service/PswdService.php
+++ app/Service/PswdService.php
@@ -2,9 +2,14 @@
 
 namespace App\Service;
 
+use App\Constant\URLConstant;
+use App\Mail\ForgetPswd;
+use App\Repositories\ForgetPswdRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class PswdService
 {
@@ -12,23 +17,58 @@ class PswdService
      * @var UserRepository
      */
     private $userRepository;
+    /**
+     * @var ForgetPswdRepository
+     */
+    private $forgetPswdRepository;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, ForgetPswdRepository $forgetPswdRepository)
     {
         $this->userRepository = $userRepository;
+        $this->forgetPswdRepository = $forgetPswdRepository;
     }
 
-    public function account(Request $request, $account)
+    public function account(Request $request)
     {
-
+        $user = $this->userRepository->findByAccount($request->input('account'));
+        if ($user != null) {
+            if (Hash::check($request->input('old_pswd'), $user->password)) {
+                $this->userRepository->update($user->id, ['password' => bcrypt($request->input('new_pswd'))]);
+                //todo log
+                return [[], Response::HTTP_NO_CONTENT];
+            } else
+                return [['error' => 'Old password error'], Response::HTTP_FORBIDDEN];
+        } else
+            return [['error' => 'The User Not Found'], Response::HTTP_NOT_FOUND];
     }
 
-    public function forget(Request $request, $email)
+    public function forget(Request $request)
     {
-
+        $user = $this->userRepository->findByEmail($request->input('email'));
+        if ($user != null) {
+            $token = md5(rand());
+            $this->forgetPswdRepository->caeate(['user_id' => $user->id, 'token' => $token]);
+            if ($request->input('redirect') == 'Frontend') {
+                if (env('APP_ENV') == 'local')
+                    $url = URLConstant::URL_DEV_FRONTEND_FORGET_PW;
+                else
+                    $url = URLConstant::URL_PDC_FRONTEND_FORGET_PW;
+                Mail::to($user)->queue(new ForgetPswd(['url' => $url . '?token=' . $user->account . '.' . $token]));
+            } elseif ($request->input('redirect') == 'AdminFrontend') {
+                if (env('APP_ENV') == 'local')
+                    $url = URLConstant::URL_DEV_ADMIN_FRONTEND_FORGET_PW;
+                else
+                    $url = URLConstant::URL_PDC_ADMIN_FRONTEND_FORGET_PW;
+                $url2 = explode('#', $url);
+                Mail::to($user)->queue(new ForgetPswd(['url' => $url2[0] . '?token=' . $user->account . '.' . $token . '#' . $url2[2]]));
+            } else
+                return [['error' => 'The Redirect Not Found'], Response::HTTP_NOT_FOUND];
+            return [[], Response::HTTP_NO_CONTENT];
+        } else
+            return [['error' => 'The User Not Found'], Response::HTTP_NOT_FOUND];
     }
 
-    public function token(Request $request, $token)
+    public function token(Request $request)
     {
 
     }
