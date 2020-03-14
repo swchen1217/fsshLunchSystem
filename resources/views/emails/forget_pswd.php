a82b487e9321e478ec4544fda2dc9a10c108c059
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-14 21:29:19 +0800

    Create forget_pswd.php

---
+++ resources/views/emails/forget_pswd.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>密碼重設確認</title>
+    <div style="max-width:700px;margin:0px auto;border:1px solid #efefef">
+        <div class="adM">
+        </div>
+        <table border="0" cellpadding="0" cellspacing="0" width="700" style="background-color: #101d40 ">
+            <tbody>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" width="700">
+            <tbody>
+            <tr>
+                <td>
+                    <img alt="" border="0" src="https://i.imgur.com/FTPsllE.png" style="width:700px" tabindex="0">
+                    <div class="a6S" dir="ltr" style="opacity: 0.01; left: 772.5px; top: 240px;">
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" width="700">
+            <tbody>
+            <tr style="text-align:right">
+                <td class="m_4575060025750704584cBlock--spacingLR" align="left" valign="top" width="100%"
+                    bgcolor="#ffffff" style="padding-left:32px;padding-right:32px;padding-top:100px;padding-bottom:4px">
+                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tbody>
+                        <tr>
+                            <td align="center" style="padding-top:4px;padding-bottom:4px"><h1
+                                    style="color:#333333;font-size:28px;font-weight:bold;font-family:'PingFang TC','\005fae\008edf\006b63\009ed1\009ad4','Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif;padding:0;margin:0;line-height:1.4;text-decoration:none;display:inline-block">
+                                    <span
+                                        style="color:#333333;text-decoration:none;font-family:'PingFang TC','\005fae\008edf\006b63\009ed1\009ad4','Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif">Hi ! 我們收到了您的密碼變更需求 </span>
+                                </h1></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tbody>
+                        <tr>
+                            <td align="center" valign="top" width="100%"
+                                style="font-family:'PingFang TC','\005fae\008edf\006b63\009ed1\009ad4','Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:17px;font-weight:normal;color:#f39733;line-height:1.4;margin:0;letter-spacing:1px;padding-top:4px;padding-bottom:4px">
+                                您可以在收到信的30分鐘內，點擊下方按鈕進行密碼重設
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table border="0" cellspacing="0" cellpadding="0" align="center" width="100%">
+                        <tbody>
+                        <tr>
+                            <td align="left" valign="top" width="100%">
+                                <div style="margin:0 auto;width:100%;max-width:640px">
+                                    <table align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                        <tr>
+                                            <td class="m_4575060025750704584cBlock--spacingLR" align="left" valign="top"
+                                                width="100%" bgcolor="#ffffff"
+                                                style="padding-left:32px;padding-right:32px;padding-top:15px;padding-bottom:100px">
+                                                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td align="center">
+                                                            <table border="0" cellspacing="0" cellpadding="0">
+                                                                <tbody>
+                                                                <tr>
+                                                                    <td align="center" bgcolor="#41a986"
+                                                                        style="border-radius:4px">
+                                                                        <a href="<?php echo $url??'';?>"
+                                                                           style="color:#ffffff;border-radius:4px;display:inline-block;text-decoration:none;font-size:16px;font-weight:bold;letter-spacing:1px;padding-top:10px;padding-bottom:10px;padding-left:60px;padding-right:60px">
+                                                                            <span
+                                                                                style="color:#ffffff;text-decoration:none;font-family:'PingFang TC','\005fae\008edf\006b63\009ed1\009ad4','Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif">更改密碼</span>
+                                                                        </a>
+                                                                    </td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table border="0" cellspacing="0" cellpadding="0" align="center" width="100%">
+                        <tbody>
+                        <tr>
+                            <td align="left" valign="top" width="100%">
+                                <div style="margin:0 auto;width:100%;max-width:640px">
+                                    <table align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                        <tr>
+                                            <td class="m_4575060025750704584cBlock--spacingLR" align="left" valign="top"
+                                                width="100%" bgcolor="#ffffff"
+                                                style="padding-left:0px;padding-right:32px;padding-top:4px;padding-bottom:15px">
+                                                <table align="center" width="100%" border="0" cellspacing="0"
+                                                       cellpadding="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td valign="top" width="100%"
+                                                            style="font-family:'PingFang TC','\005fae\008edf\006b63\009ed1\009ad4','Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:16px;font-weight:normal;color:#666666;line-height:1.5;margin:0">
+                                                            <div>
+                                                                <span
+                                                                    style="font-size:14px;color:rgb(0,0,0)">注意事項</span>
+                                                            </div>
+                                                            <div><span style="font-size:14px;color:rgb(0,0,0)">※若你無法點擊上方按鈕，</span><a
+                                                                    href="<?php echo $url??'';?>"
+                                                                    style="font-size:14px;color:rgb(27,77,225);text-decoration:underline;display:inline-block"
+                                                                    target="_blank"><span
+                                                                        style="text-decoration:underline;font-family:'PingFang TC',微軟正黑體,'Microsoft JhengHei','Helvetica Neue',Helvetica,Arial,sans-serif">請點擊我</span></a>
+                                                            </div>
+                                                            <div><span style="font-size:14px;color:rgb(0,0,0)">※如果您並未要求變更您的密碼，您可以忽略此電子郵件，<wbr>並且您的密碼將不會被變更。</span>
+                                                            </div>
+                                                            <div><span style="font-size:14px;color:rgb(0,0,0)">※若您使用上有任何疑問，歡迎聯繫客服</span>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table border="0" cellspacing="0" cellpadding="0" align="center" width="100%">
+                        <tbody>
+                        <tr>
+                            <td align="left" valign="top" width="100%">
+                                <div style="margin:0 auto;width:100%;max-width:640px">
+                                    <div><span style="color:rgb(133,133,133);font-size:14px">※此為自動發送之信件，請勿回覆。</span>
+                                    </div>
+                                    <table align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                        <tr>
+                                            <td class="m_4575060025750704584cBlock--spacingLR" align="left" valign="top"
+                                                width="100%" bgcolor="#ffffff"
+                                                style="padding-left:32px;padding-right:32px;padding-top:0px;padding-bottom:0px">
+                                                <table align="center" width="100%" border="0" cellspacing="0"
+                                                       cellpadding="0">
+                                                    <tbody>
+                                                    <tr></tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+
+            </tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" width="700">
+            <tbody>
+            <tr style="text-align:right">
+            </tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" width="700">
+            <tbody>
+            <tr style="background-color:#f4faf8;color:black;font-size:13px;line-height:15px;width:700px;padding:10px 0 10px 0;text-align:center">
+                <td style="padding:20px">
+                    <div style="font-size:15px">
+                        <a href="http://dabo.xyz/fios/privacy1.html"
+                           style="color:black;text-decoration:none;padding:0px 10px;letter-spacing:1px" target="_blank">隱私權政策</a>
+                        <a href="https://www.instagram.com/fssh_fios/"
+                           style="color:black;text-decoration:none;border-left:1px solid;padding:0px 10px;letter-spacing:1px"
+                           target="_blank">關於我們</a>
+                        <a href="http://dabo.xyz/fios/privacy1.html"
+                           style="color:black;text-decoration:none;border-left:1px solid;padding:0px 10px;letter-spacing:1px"
+                           target="_blank">服務條款</a>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px"></tr>
+            </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color:#101d40;padding:inherit" width="700">
+            <tbody>
+            <tr style="color:black;text-align:center;font-size:13px;line-height:15px">
+                <td style="width:700px;padding:40px 55px 30px"><a href="https://www.instagram.com/fssh_fios/"><img
+                            src="https://i.imgur.com/DG9aov8.png" width="20%" style="float:right"></a>
+                    <div style="text-align:left;font-size:15px"><span
+                            style="display:block;color:white;letter-spacing:1px;border-left:2px solid #eb691c;margin-bottom:10px;padding-left:15px;margin-left:30px;font-weight:400;margin-top:35px">FIOS團隊出品</span>
+                        <span
+                            style="display:block;color:white;letter-spacing:1px;border-left:2px solid #eb691c;margin-bottom:10px;padding-left:15px;margin-left:30px;font-weight:400">IG客服 : fssh_fios</span>
+                        <span
+                            style="display:block;color:white;letter-spacing:1px;border-left:2px solid #eb691c;margin-bottom:10px;padding-left:15px;margin-left:30px;font-weight:400">
+                            <wbr>Line小助手 : @140xoylw</span>
+                        <span
+                            style=" display:block;color:white;letter-spacing:1px;margin-top:35px;font-weight:400;line-height:1.4;text-align:center">
+                            © 2020 FIOS All Rights Reserved
+                            <br>
+                            鳳山高中線上訂餐系統製作團隊版權所有
+                        </span></div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</head>
+</html>
