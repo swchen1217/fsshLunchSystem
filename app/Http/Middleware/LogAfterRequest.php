82a91722b147520cbb329975507e768c270b0da7
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-16 10:57:37 +0800

    Update LogAfterRequest.php

--- app/Http/Middleware/LogAfterRequest.php
+++ app/Http/Middleware/LogAfterRequest.php
@@ -1,18 +1,20 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use Illuminate\Support\Facades\Log;
 
-class LogAfterRequest {
+class LogAfterRequest
+{
 
-    public function handle($request, \Closure  $next)
+    public function handle($request, \Closure $next)
     {
         return $next($request);
     }
 
     public function terminate($request, $response)
     {
-        Log::channel('request')->info('app.requests', ['ip' => $request->ip(),'request' => $request->all(), 'response' => $response]);
+        Log::channel('request')->info('app.requests', ['ip' => $request->ip(), 'request' => $request->all(), 'response' => $response]);
     }
 
 }
