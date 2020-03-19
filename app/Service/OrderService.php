e8b828089e79ba54e39744b3e47569ef349773b0
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-19 19:16:46 +0800

    Update OrderService.php

--- app/Service/OrderService.php
+++ app/Service/OrderService.php
@@ -233,7 +233,7 @@ class OrderService
                 throw new MyException(serialize(['error' => 'The Order Not Found']), Response::HTTP_NOT_FOUND);
             if ($this->userIdCheckWithAllAndSelfAndClass($order->user_id, 'order.modify.delete')) {
                 $this->orderRepository->delete($order_id);
-                $sale=$this->saleRepository->findById($order->sale_id);
+                $sale = $this->saleRepository->findById($order->sale_id);
                 $price = $this->dishRepository->findById($sale->dish_id)->price;
                 $balance = $this->balanceRepository->findByUserId($order->user_id);
                 if ($balance == null) {
