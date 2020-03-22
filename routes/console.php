b796fca45e3275c6cb11854610db7e0b97370d14
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-22 13:26:08 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Entity\User;
 use App\Mail\Verify;
 use App\Repositories\BalanceRepository;
 use App\Repositories\Money_logRepository;
@@ -81,22 +82,56 @@ Artisan::command('my:stu', function () {
     $this->info('ok');
 });
 
-Artisan::command('my:mailTest01 {user_id}', function (BalanceRepository $balanceRepository,Money_logRepository $money_logRepository) {
-    $money=1000;
-    $uu=App\Entity\User::find($this->argument('user_id'));
-        $uu->syncRoles('Student');
-        $balanceObj = $balanceRepository->findByUserId($uu->id);
-        if ($balanceObj != null)
-            $balance = $balanceObj->money;
-        else {
-            $balanceRepository->caeate(['user_id' => $uu->id, 'money' => 0]);
-            $balance = 0;
-        }
-        $mm = $balance + $money;
-        $balanceRepository->updateByUserId($uu->id, ['money' => $mm]);
-        $money_logRepository->caeate(['user_id' => $uu->id, 'event' => 'top-up', 'money' => $money, 'trigger_id' => 1, 'note' => $balance . '+' . $money . '=' . $mm.'(TEST01)']);
-        Log::channel('money')->info('Top up Success', ['ip' => '127.0.0.1', 'trigger_id' => 1, 'user_id' => $uu->id, 'Balance before top up' => $balance, 'Total top up' => $money, 'Balance after top up' => $mm]);
-        Mail::to($uu)->queue(new \App\Mail\TestInvite01());
-        $this->line($uu->account.' OK');
+Artisan::command('my:mailTest01 {user_id}', function (BalanceRepository $balanceRepository, Money_logRepository $money_logRepository) {
+    $money = 1000;
+    $uu = App\Entity\User::find($this->argument('user_id'));
+    $uu->syncRoles('Student');
+    $balanceObj = $balanceRepository->findByUserId($uu->id);
+    if ($balanceObj != null)
+        $balance = $balanceObj->money;
+    else {
+        $balanceRepository->caeate(['user_id' => $uu->id, 'money' => 0]);
+        $balance = 0;
+    }
+    $mm = $balance + $money;
+    $balanceRepository->updateByUserId($uu->id, ['money' => $mm]);
+    $money_logRepository->caeate(['user_id' => $uu->id, 'event' => 'top-up', 'money' => $money, 'trigger_id' => 1, 'note' => $balance . '+' . $money . '=' . $mm . '(TEST01)']);
+    Log::channel('money')->info('Top up Success', ['ip' => '127.0.0.1', 'trigger_id' => 1, 'user_id' => $uu->id, 'Balance before top up' => $balance, 'Total top up' => $money, 'Balance after top up' => $mm]);
+    Mail::to($uu)->queue(new \App\Mail\TestInvite01());
+    $this->line($uu->account . ' OK');
     $this->info('DONE');
+});
+
+Artisan::command('user:new', function () {
+    $this->line('Create User ...');
+    $account = $this->ask('Input Account: ');
+    $password = $this->secret('Input Password: ');
+    $password_c = $this->secret('Input Password: ');
+    while ($password != $password_c) {
+        $this->error('Password Not Match');
+        $password = $this->secret('Input Password: ');
+        $password_c = $this->secret('Confirm Password: ');
+    }
+    $class = $this->ask('Input Class: ');
+    $number = $this->ask('Input Number: ');
+    $name = $this->ask('Input Name: ');
+    $email = $this->ask('Input Email: ');
+    $data = ['account' => $account, 'password' => '********', 'class' => $class, 'number' => $number, 'name' => $name, 'email' => $email];
+    $headers = ['account', 'password', 'class', 'number', 'name', 'email'];
+    $this->table($headers, $data);
+    if ($this->confirm('Is the user information correct? [y|N]')) {
+        $data['password'] = bcrypt($password);
+        $uu = User::create($data);
+        $this->info('User set Success');
+        $this->info('User ID: ' . $uu->id);
+        $this->info('Account: ' . $account);
+        if ($this->confirm('Do you want to set the user role? [y|N]')) {
+            $role = $this->ask('Input Role Name (Only one): ');
+            $uu->syncRoles($role);
+            $this->info('Role set Success');
+        }
+        $this->info('DONE');
+    }
+    $this->error('Bye');
+    return false;
 });
