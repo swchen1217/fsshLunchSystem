bbadd6608cdde676ae4aec40541f3bc2fe65492e
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-30 12:34:24 +0800

    .

--- database/migrations/2020_03_30_122650_add_pw_changed_to_users_table.php
+++ database/migrations/2020_03_30_122650_add_pw_changed_to_users_table.php
@@ -14,7 +14,7 @@ class AddPwChangedToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+    	    $table->boolean('pw_changed')->after('email')->default(false);
         });
     }
 
