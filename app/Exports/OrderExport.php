1138f668dc6533d42335221ccbeb067070c49aeb
Sz-Wei Chen <swchen1217@gmail.com>
2020-04-15 16:21:10 +0800

    Update OrderExport.php

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -73,10 +73,8 @@ class OrderExport implements WithEvents
                 $event->sheet->setCellValue('J3', '總數量');
                 $event->sheet->setCellValue('K3', '總金額');
                 for ($g = 1; $g <= 3; $g++) {
-                    $event->sheet->setCellValue('B4', 'OK');
                     $r = ($g - 1) * 18;
                     for ($c = 1; $c <= 18; $c++) {
-                        $event->sheet->setCellValue('B5', 'OK');
                         $event->sheet->setCellValue('A' . ($r + $c + 3), $g . str_pad($c, 2, "0", STR_PAD_LEFT));
                     }
                 }
