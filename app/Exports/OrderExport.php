99853000c8eb560bbacbd6c929de1b1d3c420290
Sz-Wei Chen <swchen1217@gmail.com>
2020-04-16 08:37:12 +0800

    Update OrderExport.php

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -29,9 +29,9 @@ class OrderExport implements FromCollection, WithTitle, WithEvents, WithCustomSt
                 $event->sheet->getDelegate()->getPageMargins()->setBottom(1);
                 $event->sheet->getDelegate()->getPageMargins()->setRight(0.5);
                 $event->sheet->getDelegate()->getPageMargins()->setLeft(0.5);
-                
+
                 //設定列寬
-                for ($i = 0; $i <= 11; $i++) {
+                for ($i = 0; $i < 11; $i++) {
                     $event->sheet->getDelegate()->getColumnDimension($col[$i])->setWidth(8.11);
                 }
                 //設定行高，$i為資料行數
