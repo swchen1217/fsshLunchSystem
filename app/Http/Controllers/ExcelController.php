63dfd9344751c700e92a6382ff7eb385490c18da
Sz-Wei Chen <swchen1217@gmail.com>
2020-04-16 12:47:00 +0800

    Update ExcelController.php

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Exports\OrderExport;
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Validator;
 
 
 class ExcelController extends Controller
@@ -16,6 +16,11 @@ class ExcelController extends Controller
 
     public function exportOrder(Request $request, $date)
     {
+        $validator = Validator::make([$date], ['required|date_format:Y-m-d']);
+        if ($validator->fails())
+            return response()->json(['error' => 'Date format error'], Response::HTTP_BAD_REQUEST);
         Excel::store(new OrderExport($date), 'public/order/order-' . $date . '.xlsx');
+        $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        return response()->json(['url' => $protocol . $_SERVER['HTTP_HOST'] . '/storage/order/order-' . $date . '.xlsx'], 200);
     }
 }
