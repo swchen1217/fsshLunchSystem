09f9682007347da15d8071e2f22bd1fd6227932b
Sz-Wei Chen <swchen1217@gmail.com>
2020-06-28 12:11:14 +0800

    Create LineNotifyService.php

---
+++ app/Service/LineNotifyService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Carbon;
+
+class LineNotifyService
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function send($line_notify_id)
+    {
+        // return [true|flase,msg]
+
+    }
+
+}
