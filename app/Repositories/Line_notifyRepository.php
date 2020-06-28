b42993be8550ae1be8639b2592a1359cbef3953a
Sz-Wei Chen <swchen1217@gmail.com>
2020-06-28 12:10:58 +0800

    Create Line_notifyRepository.php

---
+++ app/Repositories/Line_notifyRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use App\App\Entity\Line_notify;
+
+class Line_notifyRepository
+{
+
+    /**
+     * @var Line_notify
+     */
+    private $line_notify;
+
+    public function __construct(Line_notify $line_notify)
+    {
+        $this->line_notify = $line_notify;
+    }
+
+    public function all()
+    {
+        return $this->line_notify->all();
+    }
+
+    public function findById($id)
+    {
+        return $this->line_notify->find($id);
+    }
+}
