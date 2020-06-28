6bcd2566da787b7867b90d4adda3a8684917ba25
Sz-Wei Chen <swchen1217@gmail.com>
2020-06-28 23:06:09 +0800

    Update LineNotifyService.php

--- app/Service/LineNotifyService.php
+++ app/Service/LineNotifyService.php
@@ -2,28 +2,57 @@
 
 namespace App\Service;
 
+use App\Repositories\Line_notifyRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Carbon;
+use KS\Line\LineNotify;
 
 class LineNotifyService
 {
 
-    public function __construct()
-    {
+    /**
+     * @var LineNotify
+     */
+    private $lineNotify;
+    /**
+     * @var Line_notifyRepository
+     */
+    private $line_notifyRepository;
 
+    public function __construct(LineNotify $lineNotify, Line_notifyRepository $line_notifyRepository)
+    {
+        $this->lineNotify = $lineNotify;
+        $this->line_notifyRepository = $line_notifyRepository;
     }
 
     public function send($line_notify_id)
     {
         // return [true|flase,msg]
 
+        $line_notify = $this->line_notifyRepository->findById($line_notify_id);
+        if ($line_notify == null)
+            return [false, '`line_notify_id` not found'];
+
+        $method = $line_notify->method;
+
+        return $method();
     }
 
-    private function commit($token,$message,$imageFullsize=null){
+    private function commit($token, $text, $imagePath = null, $sticker = null)
+    {
+        $this->lineNotify->setToken($token);
+        $this->lineNotify->send($text, $imagePath, $sticker);
+    }
 
+    // make message method
+
+    private function ln_1()
+    {
+        $this->commit("UWjYTfSjp4qcDjNmA24TFgIMsyqYFkxRtQIzQXdw3B4", "I Love You.");
+        return [true, 'Success'];
     }
 
 }
