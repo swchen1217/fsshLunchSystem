c10725adac4148dbe60ad6ccf35bcbed77c14664
Sz-Wei Chen <swchen1217@gmail.com>
2020-06-30 16:15:57 +0800

    Create LineNotifyMessageHandle.php

---
+++ app/Constant/LineNotifyMessageHandle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constant;
+
+class LineNotifyMessageHandle{
+    private function ln_1()
+    {
+        $this->commit("UWjYTfSjp4qcDjNmA24TFgIMsyqYFkxRtQIzQXdw3B4", "I Love You.");
+        return [true, 'Success'];
+    }
+}
