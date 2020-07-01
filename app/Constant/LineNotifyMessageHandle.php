c977fbeeab8a73fd2a3c0d25a0cba019f283904d
Sz-Wei Chen <swchen1217@gmail.com>
2020-07-01 19:42:26 +0800

    Update LineNotifyMessageHandle.php

--- app/Constant/LineNotifyMessageHandle.php
+++ app/Constant/LineNotifyMessageHandle.php
@@ -3,7 +3,7 @@
 namespace App\Constant;
 
 class LineNotifyMessageHandle{
-    private function ln_1()
+    public function ln_1()
     {
         $this->commit("UWjYTfSjp4qcDjNmA24TFgIMsyqYFkxRtQIzQXdw3B4", "I Love You.");
         return [true, 'Success'];
