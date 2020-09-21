bf93b8460bbaa2ec78511efb9efec3743c67d343
Sz-Wei Chen <swchen1217@gmail.com>
2020-09-21 21:18:45 +0800

    202009212118

--- app/Entity/Line_notify_subscribe.php
+++ app/Entity/Line_notify_subscribe.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Line_notify_subscribe extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $guarded = ['id'];
 }
--- database/migrations/2020_09_21_211502_create_line_notify_subscribes_table.php
+++ database/migrations/2020_09_21_211502_create_line_notify_subscribes_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateLineNotifySubscribesTable extends Migration
@@ -15,7 +16,13 @@ class CreateLineNotifySubscribesTable extends Migration
     {
         Schema::create('line_notify_subscribes', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('notify_id');
+            $table->string('token');
+            $table->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreign('notify_id')->references('id')->on('line_notifies')->onDelete('CASCADE');
         });
     }
 
