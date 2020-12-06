d98ee3f83c1631881cd1bd5a1e86d7d7146b5714
Sz-Wei Chen <swchen1217@gmail.com>
2020-12-06 15:36:25 +0800

    202012061536

--- init_chmod.sh
+++ init_chmod.sh
@@ -4,4 +4,5 @@ find ./ -type d -exec chmod 775 {} \;
 find ./ -type f -exec chmod 664 {} \;
 chmod 400 storage/oauth-public.key
 chmod 400 storage/oauth-private.key
+chmod 660 .env
 chmod 775 init_chmod.sh
